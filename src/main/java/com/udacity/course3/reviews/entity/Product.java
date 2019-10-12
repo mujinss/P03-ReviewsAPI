f977ca7c7a5ac0fb591feb29bd70cf308d923bb4
ss <helensunlife@gmail.com>
2019-10-12 13:15:26 -0700

    added table annotation for product entity

--- src/main/java/com/udacity/course3/reviews/entity/Product.java
+++ src/main/java/com/udacity/course3/reviews/entity/Product.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name="product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
